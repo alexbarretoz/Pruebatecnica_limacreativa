7ebbdce8df6f732c1ff8736466f17477a045efa0
alexbarretoz <alexbarreto@gmail.com>
2023-06-10 16:19:29 -0500

    index

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
 const express = require('express');
 const dotenv = require ('dotenv');
+const path = require("path")
+
  
 dotenv.config();
 const app = express();
 const port = process.env.PORT;
+//app.use(bodyParse.json());
 app.use(express.json());
-
 
 
 // inicio
 app.get('/', (req, res) => {    
-    res.json(
-        {
-            "Title": "Hola mundo"
-        }
-    );
+  res.sendFile(path.join(__dirname,"index.html"));
 });
 
 
